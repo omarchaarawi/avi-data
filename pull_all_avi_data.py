1f983604f0a628fcb4ed6c0eecd338ff9e630ff3
omarchaarawi <omarchaarawi@gmail.com>
2020-10-04 13:47:31 -0600

    Optimized getAviData to increase speed about 25x

---
+++ pull_all_avi_data.py
@@ -0,0 +1,28 @@
+import re
+from datetime import datetime, date
+import requests
+import lxml.html as lh
+from helpers import getAviData
+import time
+
+# Get today's date so it can be passed to getAviData(zone_id, str date_of_pull)
+date = str(date.today())
+
+# Iterate through all zones
+for i in range(0,10):
+    # Might as well time this to see how long each pull per zone takes
+    start = int(time.time())
+    
+    # This is the work-horse that gets the data
+    getAviData(i,date)
+    
+    finish = int(time.time())
+    elapsed_time = (finish - start)/60
+    print("zone {0} took {1:.2f} minutes".format(i, elapsed_time))
+    with open(date + "_time_to_collect_data.txt", "a") as file:
+        file.write("zone {0} took {1:.2f} minutes\n".format(i, elapsed_time))
+    
+    # Sleep 5 minutes between pulls to give CAIC servers a break
+    # I don't know if this helps...
+    if(i != 9):
+        time.sleep(300)
